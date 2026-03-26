8bce52202831aff96fcf84ba66855aff4e03e27f
Riq <ruylivinginadream@duck.com>
2026-03-26 11:57:06 -0300

    .

--- components/CustomDrawerComponents.tsx
+++ components/CustomDrawerComponents.tsx
@@ -1,8 +1,15 @@
 import { DrawerContentComponentsProps, DrawerContentScrollView } from '@react-navigation/drawer';
 import { View, Text } from 'react-native';
 
+const ITEMS = [
+    { label: 'Perfil', screen: 'perfil' },
+    { label: 'Conquistas', screen: 'conquistas' },
+    { label: 'Histórico', screen: 'historico' },
+    { label: 'Sair', screen: 'sair' },
+]
+
 export function CustomDrawerContent(props: DrawerContentComponentsProps) {
     return (
-        
+        // lista de itens do drawer
     )
 }
